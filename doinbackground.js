5e810d7f8acc9da7cc47fdc171c7a1656a77c292
Steven Truong <steventr@usc.edu>
2017-05-03 00:50:25 -0700

    Notifications

--- doinbackground.js
+++ doinbackground.js
@@ -2,7 +2,18 @@ setInterval(function(){
 		console.log("WEEEEEE"); 
         $.get("http://sample-env-1.cegpykp7aq.us-east-1.elasticbeanstalk.com?userid=1337", function(data) {
 	    	if(data.modified=="true"){
-	    		alert("changed");
+				var notification = chrome.notifications.create('Notification', { 
+					type: "basic", 
+					iconUrl: "icon.png",
+					title: "Change detected!",
+					message: "Test"
+					}, 
+						function(id) {
+							if(chrome.runtime.lastError) {
+							console.error(chrome.runtime.lastError.message);
+							}
+					}
+				);
 	    	}
 	    });
 }, 6000);
