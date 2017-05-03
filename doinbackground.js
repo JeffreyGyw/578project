ecd3b0bf53f2ea1f589cdbc8e17b13b3e4238741
Steventr <steventr@usc.edu>
2017-05-03 11:42:26 -0700

    Proper showing notification message

--- doinbackground.js
+++ doinbackground.js
@@ -1,19 +1,21 @@
 setInterval(function(){
         $.get("http://sample-env-1.cegpykp7aq.us-east-1.elasticbeanstalk.com?userid=1338", function(data) {
 	    	if(data.modified=="true"){
-				console.log(data); 
-				var notification = chrome.notifications.create('Notification', { 
+				for (var i = 0; i < data.results.length; i++) {
+					console.log(data.results[i]); 
+					var notification = chrome.notifications.create('Notification', { 
 					type: "basic", 
 					iconUrl: "icon.png",
 					title: "Change detected!",
-					message: "HTML ID: " + data.elementid + " at URL: " + data.url,
+					message: "HTML ID: " + data.results[i].elementid + " at URL: " + data.results[i].url,
 					}, 
 						function(id) {
-							if(chrome.runtime.lastError) {
-							console.error(chrome.runtime.lastError.message);
-							}
-					}
-				);
+								if(chrome.runtime.lastError) {
+								console.error(chrome.runtime.lastError.message);
+								}
+						}
+					);
+				}
 	    	}
 	    });
 }, 6000);
